c5e1ba53760a7163ba2746dba0919aca17bf48b6
AmmadRaza1993 <ammadsiddiqui136@gmail.com>
2023-12-21 19:28:49 +0500

    .

---
+++ ISupportWeb/Controllers/Shared.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ISupportWeb.Controllers
+{
+    public class Shared : Controller
+    {
+        public IActionResult Header()
+        {
+            return PartialView("Header");
+        }
+        public IActionResult MobileNavigation()
+        {
+            return PartialView("MobileNavigation");
+        }
+    }
+}
---
+++ ISupportWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISupportWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
